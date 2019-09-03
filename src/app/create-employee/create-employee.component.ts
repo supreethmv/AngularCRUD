7358286884daa9ba9599066036852ebf77b1e0b5
supreethmv <supreethmv@gmail.com>
2019-09-03 22:36:42 +0200

    v50

---
+++ src/app/create-employee/create-employee.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Department } from '../models/department.model';
+import { Employee } from '../models/employee.model';
+import { EmployeeService } from '../employee.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent implements OnInit {
+  departments: Department[] = [
+    { id: 1, name: 'Help Desk' },
+    { id: 2, name: 'HR' },
+    { id: 3, name: 'IT' },
+    { id: 4, name: 'Payroll' }
+  ];
+  previewPhoto = false;
+  employee: Employee = {
+    id: null,
+    name: null,
+    gender: null,
+    contactPreference: null,
+    phoneNumber: null,
+    email: null,
+    dateOfBirth: null,
+    department: '-1',
+    isActive: null,
+    photoPath: null
+  };
+  constructor(private _employeeService: EmployeeService,private _router: Router) { }
+
+  ngOnInit() {
+  }
+  saveEmployee(): void {
+    this._employeeService.save(this.employee);
+    this._router.navigate(['list']);
+  }
+  togglePhotoPreview() {
+    this.previewPhoto = !this.previewPhoto;
+  }
+
+}
---
+++ src/app/list-employees/list-employees.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../models/employee.model';
+import { EmployeeService } from '../employee.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-list-employees',
+  templateUrl: './list-employees.component.html',
+  styleUrls: ['./list-employees.component.css']
+})
+export class ListEmployeesComponent implements OnInit {
+
+  employees:Employee[];
+
+
+  constructor(private _employeeService: EmployeeService, private _router: Router) { }
+
+  ngOnInit() {
+    this.employees = this._employeeService.getEmployees();
+  }
+
+  onClick(employeeId: number){
+    this._router.navigate(['/employees', employeeId]);
+  }
+
+}
